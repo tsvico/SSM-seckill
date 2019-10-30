df08eeb78d8719acaeb97ce352e851e82c93bd77
tsvico <tsxygwj@163.com>
2019-10-30 16:10:02 +0800

    重新导出数据库SQL

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -1,44 +1,60 @@
--- 数据库初始化脚本
+/*
+Navicat MySQL Data Transfer
 
--- 创建数据库
-CREATE DATABASE seckill;
--- 使用数据库
-use seckill;
--- 创建秒杀数据库
-create table seckill
-(
-    `seckill_id`  bigint       NOT NULL AUTO_INCREMENT COMMENT '商品库存ID',
-    `name`       varchar(120) not null comment '商品名称',
-    `number`     int          not null comment '库存数量',
-    `start_time`  timestamp    not null comment '秒杀开始时间',
-    `end_time`   timestamp    not null comment '秒杀结束时间',
-    `create_time` timestamp    not null default current_timestamp comment '创建时间',
-    primary key (seckill_id),
-    key idx_start_time (start_time),
-    key idx_end_time (end_time),
-    key idx_create_time (create_time)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1000
-  DEFAULT CHARSET = utf8 COMMENT ='秒杀库存表';
+Source Server         : localhost_3306
+Source Server Version : 80012
+Source Host           : localhost:3306
+Source Database       : seckill
 
--- 初始化数据
-insert into
-    seckill(name, number, start_time, end_time)
-values
-       ('1000秒杀iPhoneX',100,'2019-11-01 00:00:00','2019-11-02 00:00:00'),
-       ('500秒杀小米MIX4',10,'2019-11-01 00:00:00','2019-11-02 00:00:00'),
-       ('300秒杀小米9',100,'2019-11-01 00:00:00','2019-11-02 00:00:00'),
-       ('200秒杀大西瓜',100,'2019-11-01 00:00:00','2019-11-02 00:00:00');
+Target Server Type    : MYSQL
+Target Server Version : 80012
+File Encoding         : 65001
 
--- 秒杀成功明细表
--- 用户登录认证相关信息
-create table success_kill(
-    `seckill_id` bigint not null comment '秒杀商品ID',
-    `user_phone` bigint not null comment '用户手机号',
-    `state` tinyint not null default -1 comment '状态表示:-1无效 0 成功 1 已付款 2 已发货',
-    `create_time` timestamp not null comment '创建时间',
-    primary key (seckill_id,user_phone), /*联合主键*/
-    key idx_create_time(create_time)
-)ENGINE = InnoDB DEFAULT CHARSET = utf8 COMMENT ='秒杀成功明细表'
+Date: 2019-10-30 16:09:00
+*/
 
--- 锻炼手写sql能力
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for seckill
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill`;
+CREATE TABLE `seckill` (
+  `seckill_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品库存ID',
+  `name` varchar(120) NOT NULL COMMENT '商品名称',
+  `number` int(11) NOT NULL COMMENT '库存数量',
+  `start_time` timestamp NOT NULL COMMENT '秒杀开始时间',
+  `end_time` timestamp NOT NULL COMMENT '秒杀结束时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`seckill_id`),
+  KEY `idx_start_time` (`start_time`),
+  KEY `idx_end_time` (`end_time`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
+
+-- ----------------------------
+-- Records of seckill
+-- ----------------------------
+INSERT INTO `seckill` VALUES ('1000', '1000秒杀iPhoneX', '96', '2019-10-31 00:00:00', '2019-11-01 00:00:00', '2019-10-23 15:57:43');
+INSERT INTO `seckill` VALUES ('1001', '500秒杀小米MIX4', '5', '2019-10-30 14:20:00', '2019-11-02 00:00:00', '2019-10-09 15:57:43');
+INSERT INTO `seckill` VALUES ('1002', '300秒杀小米9', '100', '2019-11-01 00:00:00', '2019-11-02 00:00:00', '2019-10-09 15:57:43');
+INSERT INTO `seckill` VALUES ('1003', '200秒杀大西瓜', '100', '2019-11-01 00:00:00', '2019-11-02 00:00:00', '2019-10-09 15:57:43');
+
+-- ----------------------------
+-- Table structure for success_kill
+-- ----------------------------
+DROP TABLE IF EXISTS `success_kill`;
+CREATE TABLE `success_kill` (
+  `seckill_id` bigint(20) NOT NULL COMMENT '秒杀商品ID',
+  `user_phone` bigint(20) NOT NULL COMMENT '用户手机号',
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态表示:-1无效 0 成功 1 已付款 2 已发货',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`seckill_id`,`user_phone`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
+
+-- ----------------------------
+-- Records of success_kill
+-- ----------------------------
+INSERT INTO `success_kill` VALUES ('1001', '15588537323', '0', '2019-10-30 14:50:40');
+INSERT INTO `success_kill` VALUES ('1001', '18005385482', '0', '2019-10-30 14:50:40');
